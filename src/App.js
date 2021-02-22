f9c0199adcf384089ad3104225028d4ec04866e8
tpo-jrallen2 <77024049+tpo-jrallen2@users.noreply.github.com>
2021-02-22 08:34:47 -0800

    Commit monday changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import apiKey from './config.js';
+
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+
+
+const key = apiKey;
+
+export default class  App extends Component {
+
+  constructor() {
+    super(); 
+    this.state = {
+      photos: [],
+      loading: true
+    };
+  }
+
+  getFlickrResults = () => {
+    // call the flickr api 
+    axios.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=256b0dba48f3cbd0009b06773fe551e6&tags=waterfall&safe_search=1&format=json&nojsoncallback=1&per_page=24')
+      .then( response => {
+        const photoData = response.data.photos.photo;
+        // loop through all photos 
+        photoData.map( photo => {
+          const photoLink = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`;
+          // add the built photo link to state to be used to display on the page 
+          this.setState(prevState => ({
+            photos: [...prevState.photos, photoLink]
+          }))
+        });
+      })
+      .then(this.setState({ loading: false }))
+      .catch( err => console.log(err));
+
+  }
+
+  componentDidMount() {
+    this.getFlickrResults();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <SearchBar />
+
+      </div>
+    );
+  }
 }
-
-export default App;
